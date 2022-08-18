6debea51f198bad87ab5445a78a39ef23d947567
Adrian <adrian.talonia11@gmail.com>
2022-08-17 23:02:45 -0500

    add test controller

---
+++ src/main/kotlin/com/kotlinapi/controller/TestController.kt
@@ -0,0 +1,15 @@
+package com.kotlinapi.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/")
+class TestController {
+
+    @GetMapping
+    fun get(): String {
+        return "Hi Colorado!!!"
+    }
+}
